307a4c4f4ef48e1e3be4125648cd9b7c3982236b
aymenbakkour <76061720+aymenbakkour@users.noreply.github.com>
2025-11-28 06:29:15 +0100

    Add initial product list in products.js

---
+++ products.js
@@ -0,0 +1,18 @@
+// ملف المنتجات فقط
+
+const products = [
+
+    { id:"p1", category:"مواد غذائية", name:"٥ كغ سكر", price:3.75 },
+    { id:"p2", category:"مواد غذائية", name:"٥ كغ رز مصري", price:5 },
+    { id:"p3", category:"مواد غذائية", name:"٥ كغ رز تايلندي", price:5 },
+    { id:"p4", category:"مواد غذائية", name:"٢ كغ دبس بندورة", price:3 },
+    { id:"p5", category:"مواد غذائية", name:"٢ كغ سمنة كلارنا", price:5.85 },
+    { id:"p6", category:"مواد غذائية", name:"٤ لتر زيت دوار الشمس", price:8.35 },
+    { id:"p7", category:"مواد غذائية", name:"٢ كغ برغل خشن", price:1.7 },
+    { id:"p8", category:"مشروبات",      name:"٤٥٠ غ شاي سيدي هشام", price:5.5 },
+    { id:"p9", category:"مشروبات",      name:"٥٠٠ غ بن الحموي هال اكسترا", price:8.5 },
+    { id:"p10", category:"مواد غذائية", name:"٥ قوالب ماجي", price:1 },
+    { id:"p11", category:"لحوم", name:"٢ كغ فخاد فروج", price:5 },
+    { id:"p12", category:"لحوم", name:"١ كغ لحمة عجل هبرة", price:12 }
+
+];
